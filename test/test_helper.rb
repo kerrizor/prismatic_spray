bad8131e00b205dd413ebfb295ad30cb5a86062b
Kerri Miller <kerrizor@kerrizor.com>
2014-10-11 10:18:19 -0400

    add basic test_helper

---
+++ test/test_helper.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/unit'
+require 'minitest/pride'
+
+require './lib/prismatic_spray'
+
+class MiniTest::Unit::TestCase
+  def assert_change(block)
+    before = block.call
+
+    yield
+
+    refute_equal before, block.call
+  end
+
+  def refute_change(block)
+    before = block.call
+
+    yield
+
+    assert_equal before, block.call
+  end
+end
